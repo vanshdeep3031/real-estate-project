f752c400b87fda3df84eac912f2707651dbc1018
vanshdeep3031 <vansh.0112deep@gmail.com>
2026-06-02 00:33:45 +0530

    rebrand to Dhillon Properties

--- server/index.js
+++ server/index.js
@@ -8,7 +8,11 @@ dotenv.config();
 const app = express();
 
 // Middleware
-app.use(cors({ origin: "https://real-estate-project-five-omega.vercel.app" }));
+app.use(cors({
+  origin: "https://real-estate-project-five-omega.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
